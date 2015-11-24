005a5d8167468900204305a0fa183cc6df66c1db
diwasblack <diwasblack@gmail.com>
2015-11-24 18:20:17 +0545

    Bash script to install terminal enviroment

---
+++ env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create directory for keeping old files
+if [ ! -d $HOME/oldfiles ]; then
+    mkdir $HOME/oldfiles
+fi
+
+# Move previous files to another directory
+names=( "tmux.conf" "zshrc" )
+files=( ".tmux.conf" ".zshrc" )
+for i in `seq 0 1`; do
+    if [ -f $HOME/${files[$i]} ]; then
+        mv "$HOME/${files[$i]}" "$HOME/oldfiles/"
+    fi
+    ln -s $HOME/dotfiles/${names[$i]} $HOME/${files[$i]}
+done
