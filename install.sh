d986998987d5efcb126e85a7a98852a580d03963
Diwas Sharma <diwasblack@gmail.com>
2017-03-23 17:33:49 +0545

    Added silversearcher-ag installation to install.sh

--- install.sh
+++ install.sh
@@ -47,6 +47,9 @@ echo "############ Downloading vim-plug ############"
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "############ Installing silver searcher ############"
+sudo apt install silversearcher-ag
+
 echo "############ Installing python libraries ############"
 sudo -H /usr/bin/pip3 install psutil
 sudo -H /usr/bin/pip3 install neovim
