430ecaaac50d6b931bf22951e609ad4795cbcd05
Diwas Sharma <diwasblack@gmail.com>
2017-09-01 20:58:02 -0500

    Added bash script to customize ubuntu

---
+++ customize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Customization script for ubuntu
+
+# First install unity-tweak-tools
+sudo apt install -y unity-tweak-tool
+
+# Install theme to use
+sudo apt install -y arc-theme
+
+# Install redshift and redshift-gtk for display
+sudo apt install -y redshift redshift-gtk
