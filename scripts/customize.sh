ab03f29821500703646ae3354cce4fe2bc34ff5d
Diwas Sharma <diwasblack@gmail.com>
2017-10-19 12:56:54 -0500

    Updated ubuntu customization script

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -2,13 +2,11 @@
 
 # Customization script for ubuntu
 
-source ~/dotfiles/scripts/vars.sh
+# Declare variables to use
+export pkg_mgr_install="sudo apt install -y"
 
 echo -e "\n############ Installing unity-tweak-tool ############"
 ${pkg_mgr_install} unity-tweak-tool
 
 echo -e "\n############ Installing arc-theme ############"
 ${pkg_mgr_install} arc-theme
-
-echo -e "\n############ Installing redshift ############"
-${pkg_mgr_install} redshift redshift-gtk
